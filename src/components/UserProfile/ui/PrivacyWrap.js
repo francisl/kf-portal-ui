0b262022b5618ed008c6a113aea127fb5fbef464
Velythyl <charlie.gauthier@umontreal.ca>
2019-07-09 11:44:36 -0400

    progress on Privacy Wrap

--- src/components/UserProfile/ui/PrivacyWrap.js
+++ src/components/UserProfile/ui/PrivacyWrap.js
@@ -1,19 +1,31 @@
 import * as React from 'react';
 import { H4 } from '../../../uikit/Headings';
 
+/**
+ * Wraps the children of the Privacy Wrap into a if-else if-else:
+ *
+ * IF editing: return the children
+ * ELSE IF the accessor is an array:
+ *  for every empty accessed value, display a "No X value found". If applicable, display the children.
+ * ELSE IF the accessed value is empty (and the accessor is a string): display a "No X value found".
+ * ELSE return the children.
+ *
+ * By "empty" we mean: the accessors used on the profile return an empty value (undefined, empty array, null, "", etc)
+ *
+ * @param profile
+ * @param accessor
+ * @param editing
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
 const PrivacyWrap = ({profile, accessor, editing=false, ...props}) => {
 
-  console.log(profile)
-  console.log(accessor);
-  function None({acc}) {
-    return <H4>No <span style={{textTransform: 'capitalize'}}>{acc}</span> found for this profile.</H4>
-  }
+  const None = ({acc}) => <H4>No {acc} found for this profile.</H4>;
 
   function isEmpty(acc) {
-    return isInvalid(profile[acc]);
-  }
+    const value = profile[acc];
 
-  function isInvalid(value) {
     return value === null || value === undefined || (Array.isArray(value) && value.length === 0) || value === "";
   }
 
@@ -22,24 +34,51 @@ const PrivacyWrap = ({profile, accessor, editing=false, ...props}) => {
   if (Array.isArray(accessor)) {
     const invalids = accessor.filter(isEmpty);
 
-    if (invalids.length !== 0) return <div><None acc={invalids.map(toSpaceCase).join(", ")}/>{props.children}</div>;
-  } else {
-    if (isEmpty(accessor)) return <None acc={accessor}/>
-  }
+    if(invalids.length === accessor.length) return <div><None acc={joinWithLast(invalids)}/></div>;
+    else if(invalids.length !== 0) return <div><None acc={joinWithLast(invalids)}/>{props.children}</div>;
+
+  } else if (isEmpty(accessor)) return <None acc={accessor}/>;
+
 
   return props.children;
 };
 
 export default PrivacyWrap;
 
-function toSpaceCase(item) {
+/**
+ * Takes an array of camelCase words and returns a string of space case words, separated by ", " and the final by
+ * lastSep
+ *
+ * @param arr
+ * @param lastSep
+ * @returns {string|*}
+ */
+function joinWithLast(arr, lastSep=" or ") {
+  if(arr.length === 1) return arr[0];
+
+  const copy = [...arr];
+
+  const lastItem = toSpaceCase(copy.pop());
+  const joined = copy.map(toSpaceCase).join(", ");
+
+  return `${joined}${lastSep}${lastItem}`;
+}
+
+/**
+ * Returns the space case conversion of a camel case word
+ * @param word
+ * @returns {string|string}
+ */
+function toSpaceCase(word) {
   let spaced = "";
 
-  for(let i=0; i<item.length; i++) {
-    const char = item[i];
+  for(let i=0; i<word.length; i++) {
+    const char = word.charAt(i);
 
     if(char === char.toUpperCase()) spaced += " ";
-    spaced += char.toLowerCase();
+
+    if(i === 0) spaced += char.toUpperCase();
+    else spaced += char;
   }
 
   return spaced;
